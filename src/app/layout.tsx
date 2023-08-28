5e726597d45695cd22eb12557775b9cb93c49723
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-08-28 17:25:57 +0200

    Layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 "use client";
 import Navbar from "@/components/nav/Navbar";
 import "./globals.css";
-export const metadata = {
-  title: "NextJS template with TypeScript, TailwindCSS, and MongoDB",
-  description:
-    "NextJS template with TypeScript, TailwindCSS, and MongoDB, created by @clipper.",
-};
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Sidebar from "@/components/nav/Sidebar";
 import { RecoilRoot } from "recoil";
@@ -18,6 +13,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
+        <head>
+          <title>Bloggify</title>
+        </head>
         <RecoilRoot>
           <body className="w-full h-screen flex flex-col bg-gray-50">
             <Navbar />
